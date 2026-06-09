4423d44b21eca0184c3984252593231183a1665a
Polina-KD <you@example.com>
2026-06-09 11:50:05 +0200

    Add Order model

--- report/models.py
+++ report/models.py
@@ -1,6 +1,26 @@
-def main(name):
-    pass
+from dataclasses import dataclass
+import datetime
 
 
-if __name__ == '__main__':
-    main()
+@dataclass
+class Order:
+    """
+    Represents a single order from WMS export CSV file.
+
+    Attributes:
+        order_id: Unique order ID.
+        date: Date when the order was placed.
+        customer: Customer full name.
+        product: Name of the product ordered.
+        category: Product category.
+        quantity: Numbers of items ordered.
+        price: Price per single product.
+    """
+    order_id: int
+    date: datetime.date
+    customer: str
+    product: str
+    category: str
+    quantity: int
+    price: float
+
