30bb38b8f24dfdc51bd8b24f5ce34942276f3443
Polina-KD <you@example.com>
2026-06-10 13:50:50 +0200

    Improve logging and error handling

--- report/exceptions.py
+++ report/exceptions.py
@@ -2,17 +2,15 @@ class InvalidCsvError(Exception):
     """
     Raised when the CSV contains invalid data.
     """
-class InvalidPriceFormat(Exception):
-    """
-    Raised when the price format is invalid.
-    """
 
-class MissingHeaderError(Exception):
-    """
-    Raised when the CSV contains missing headers.
-    """
 
 class MissingColumnError(Exception):
     """
     Raised when a required CSV column is missing.
+    """
+
+
+class UnsupportedFileFormat(Exception):
+    """
+    Raised when requested file format is not supported.
     """
